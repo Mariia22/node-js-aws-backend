95fcf385aaee1776e03232e98677256ca05390c5
Maria22 <m.ushakova22@gmail.com>
2023-06-02 15:29:10 +0200

    fix: products description

--- product-service/mockData/products.ts
+++ product-service/mockData/products.ts
@@ -1,31 +1,24 @@
 export const products = [
   {
-    description: `Oversized, calf-length dress in woven fabric with a deep V-neckline and narrow drawstrings 
-    under the bust that tie at the sides. Long, voluminous balloon sleeves in a raglan cut with narrow elastication 
-    at the cuffs. Unlined.`,
+    description: `Oversized, calf-length dress in woven fabric with a deep V-neckline and narrow drawstrings under the bust that tie at the sides. Long, voluminous balloon sleeves in a raglan cut with narrow elastication at the cuffs. Unlined.`,
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
     price: 29.99,
     title: "Oversized tie-detail dress"
   },
   {
-    description: `Short, fitted dress in a viscose crêpe weave with a V-shaped neckline and narrow, adjustable 
-    shoulder straps. Flounce trim at the top that continues down the front and back, and a flounce-trimmed hem. 
-    Unlined.`,
+    description: `Short, fitted dress in a viscose crêpe weave with a V-shaped neckline and narrow, adjustable shoulder straps. Flounce trim at the top that continues down the front and back, and a flounce-trimmed hem. Unlined.`,
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
     price: 18.99,
     title: "Flounce-trimmed crêpe dress"
   },
   {
-    description: `Calf-length, sleeveless dress in woven fabric with narrow, adjustable shoulder straps. Deep 
-    V-neckline and double spaghetti drawstrings at the waist, tied at the sides. Gently flared skirt. Unlined.`,
+    description: `Calf-length, sleeveless dress in woven fabric with narrow, adjustable shoulder straps. Deep V-neckline and double spaghetti drawstrings at the waist, tied at the sides. Gently flared skirt. Unlined.`,
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
     price: 18.99,
     title: "Drawstring-detail dress"
   },
   {
-    description: `Short, oversized dress in woven fabric made from a lyocell blend. Deep, gathered V-neckline with 
-    spaghetti ties at the front. 3/4-length balloon sleeves in a raglan cut with narrow elastication at the cuffs. 
-    Seams with delicate piping at the waist and hips. Unlined.`,
+    description: `Short, oversized dress in woven fabric made from a lyocell blend. Deep, gathered V-neckline with spaghetti ties at the front. 3/4-length balloon sleeves in a raglan cut with narrow elastication at the cuffs. Seams with delicate piping at the waist and hips. Unlined.`,
     id: "7567ec4b-b10c-48c5-9345-fc73348a80a1",
     price: 39.99,
     title: "Oversized lyocell-blend dress"

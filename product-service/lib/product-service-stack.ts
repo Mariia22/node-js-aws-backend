4baf4d2caa7928a04fcb447843693986351e9a61
Maria22 <m.ushakova22@gmail.com>
2023-06-02 14:47:36 +0200

    feat: add products endpoint

--- product-service/lib/product-service-stack.ts
+++ product-service/lib/product-service-stack.ts
@@ -1,9 +1,34 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as apigwv from "@aws-cdk/aws-apigatewayv2-alpha";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as path from "path";
+import { shareLambdaProps } from "../utils";
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    const getProducts = new NodejsFunction(this, "GetProductsLambda", {
+      ...shareLambdaProps,
+      functionName: "getProducts",
+      entry: path.join(__dirname, "..", "handlers", "getProducts.ts")
+    });
 
+    // Defines API Gateway
+    const httpApi = new apigwv.HttpApi(this, "ProductApi", {
+      corsPreflight: {
+        allowHeaders: ["*"],
+        allowOrigins: ["*"],
+        allowMethods: [apigwv.CorsHttpMethod.GET]
+      }
+    });
+
+    httpApi.addRoutes({
+      path: "/products",
+      methods: [apigwv.HttpMethod.GET],
+      integration: new HttpLambdaIntegration("Get products", getProducts)
+    });
   }
 }

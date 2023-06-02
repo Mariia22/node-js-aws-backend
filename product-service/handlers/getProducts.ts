6db7c81fa3d85f0eed9d6e8ed1a38d824e39f1f4
Maria22 <m.ushakova22@gmail.com>
2023-06-02 15:26:35 +0200

    feat: add products information

--- product-service/handlers/getProducts.ts
+++ product-service/handlers/getProducts.ts
@@ -1,7 +1,7 @@
 import { products } from "./../mockData/products";
 import { response } from "../utils";
 
-export const handler = async (event: any) => {
+export const handler = async () => {
   try {
     return response(200, { products });
   } catch (err: any) {
--- product-service/mockData/products.ts
+++ product-service/mockData/products.ts
@@ -1,38 +1,45 @@
 export const products = [
   {
-    description: "Short Product Description1",
+    description: `Oversized, calf-length dress in woven fabric with a deep V-neckline and narrow drawstrings 
+    under the bust that tie at the sides. Long, voluminous balloon sleeves in a raglan cut with narrow elastication 
+    at the cuffs. Unlined.`,
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
-    price: 24,
-    title: "ProductOne",
+    price: 29.99,
+    title: "Oversized tie-detail dress"
   },
   {
-    description: "Short Product Description7",
+    description: `Short, fitted dress in a viscose crêpe weave with a V-shaped neckline and narrow, adjustable 
+    shoulder straps. Flounce trim at the top that continues down the front and back, and a flounce-trimmed hem. 
+    Unlined.`,
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
-    price: 15,
-    title: "ProductTitle",
+    price: 18.99,
+    title: "Flounce-trimmed crêpe dress"
   },
   {
-    description: "Short Product Description2",
+    description: `Calf-length, sleeveless dress in woven fabric with narrow, adjustable shoulder straps. Deep 
+    V-neckline and double spaghetti drawstrings at the waist, tied at the sides. Gently flared skirt. Unlined.`,
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
-    price: 23,
-    title: "ProductTwo",
+    price: 18.99,
+    title: "Drawstring-detail dress"
   },
   {
-    description: "Short Product Description4",
+    description: `Short, oversized dress in woven fabric made from a lyocell blend. Deep, gathered V-neckline with 
+    spaghetti ties at the front. 3/4-length balloon sleeves in a raglan cut with narrow elastication at the cuffs. 
+    Seams with delicate piping at the waist and hips. Unlined.`,
     id: "7567ec4b-b10c-48c5-9345-fc73348a80a1",
-    price: 15,
-    title: "ProductTest",
+    price: 39.99,
+    title: "Oversized lyocell-blend dress"
   },
   {
-    description: "Short Product Descriptio1",
+    description: `Calf-length, fitted skirt in softly draping satin with a high, elasticated waist and an asymmetric hem. Unlined.`,
     id: "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
-    price: 23,
-    title: "Product2",
+    price: 19.99,
+    title: "Asymmetric satin skirt"
   },
   {
-    description: "Short Product Description7",
+    description: `Cropped off-the-shoulder top in woven fabric made from a viscose blend. Smocked bodice and short, double-layered balloon sleeves with narrow elastication at the cuffs.`,
     id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
-    price: 15,
-    title: "ProductName",
-  },
-]
+    price: 14.99,
+    title: "Smocked off-the-shoulder top"
+  }
+];

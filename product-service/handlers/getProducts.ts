7e3ec3add8ff2305c2f8ceb857a5ca2e5a7ef891
Maria22 <m.ushakova22@gmail.com>
2023-06-05 13:07:40 +0200

    fix: products answer

--- product-service/handlers/getProducts.ts
+++ product-service/handlers/getProducts.ts
@@ -3,7 +3,7 @@ import { response } from "../utils";
 
 export const handler = async () => {
   try {
-    return response(200, { products });
+    return response(200, products );
   } catch (err: any) {
     return response(500, { message: err });
   }

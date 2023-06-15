d34208d45dd5fabeb862129392626929292ed308
Maria22 <m.ushakova22@gmail.com>
2023-06-15 17:05:46 +0200

    feat: add lambda function

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,16 +1,33 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+import { shareLambdaProps } from "./../../product-service/utils/index";
+import * as cdk from "aws-cdk-lib";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+import * as path from "path";
+import * as apigwv from "@aws-cdk/aws-apigatewayv2-alpha";
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const importProducts = new NodejsFunction(this, "importProductsFile", {
+      ...shareLambdaProps,
+      functionName: "importProducts",
+      entry: path.join(__dirname, "..", "handlers", "importProducts.ts")
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImportServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const httpApi = new apigwv.HttpApi(this, "ImportProductsApi", {
+      corsPreflight: {
+        allowHeaders: ["*"],
+        allowOrigins: ["*"],
+        allowMethods: [apigwv.CorsHttpMethod.GET]
+      }
+    });
+
+    httpApi.addRoutes({
+      path: "/import",
+      methods: [apigwv.HttpMethod.GET],
+      integration: new HttpLambdaIntegration("Import products", importProducts)
+    });
   }
 }
